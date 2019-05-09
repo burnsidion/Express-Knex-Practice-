89b697e6ae81185b68502751c660dafe3d8e2823
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2019-05-09 14:20:31 -0600

    added cors and http request permissions

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-const messages = require('./routes/newmessages')
+const messages = require('./routes/newmessages');
 const createError = require('http-errors');
 const logger = require('morgan');
 
-app.use(bodyParser.json())
+
+app.use(cors());
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join('.')));
-app.use('/newmessages', messages);
+app.use('/messages', messages);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 const port = process.env.PORT || 3001;
 
